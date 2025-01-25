97be2595d4c174c76b98b75ae685ee82e80dd6d7
stas115gr <stashauzkin10@gmail.com>
2025-01-25 13:22:04 +0200

    task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    for (int j = 1; j < 11; j++)
+            //    Console.Write($"{j}\t");
+            //    Console.WriteLine();
+            //    Console.ReadLine();
+            //}
+
+          
+            //for (int i = 4; i < 8; i++)
+            //{
+            //    for (int j = 1; j < 11; j++)
+            //    Console.Write($"{i*10+j}\t");
+            //    Console.WriteLine();
+            //    Console.ReadLine();
+            //}
+
+            
+            //for(int i=1; i<=5; i++)
+            //{
+            //    for(int j=0; j<i; j++)
+            //    Console.Write("5\t");
+            //    Console.WriteLine();
+            //    Console.ReadLine();
+            //}
+
+             
+            //for (int i = 5; i > 0; --i)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    Console.Write("1");
+            //    Console.WriteLine();
+            //    Console.ReadLine();
+            //}
+
+        }
+    }
+}
